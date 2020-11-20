1228c9a08989400da570ba562bf84f8fe280d43e
“Kajol7052” <kajol7052@gmail.com>
2020-11-20 16:55:13 +0530

    UC-2 To Calculate Daily Employee Wage

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -5,3 +5,10 @@ echo "Welcome to Employee Wages Computation Program"
 
 # UC-1 Check Employee Attendance
 Attendance=$((1+RANDOM%2))
+
+#UC-2 Calculate Daily Employee Wages
+EMP_RATE_PER_HOUR=20
+FULL_TIME=8
+WORKING_HOUR=$FULL_TIME
+DAILY_WAGE=$((EMP_RATE_PER_HOUR*WORKING_HOUR))
+echo "Daily Wages : $DAILY_WAGE"
