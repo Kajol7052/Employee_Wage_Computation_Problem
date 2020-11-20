d3e0dc9c75d896bbc56c4af3b4e2a18a4eca8d54
“Kajol7052” <kajol7052@gmail.com>
2020-11-20 18:44:30 +0530

    Refactored : UC-9 To store Day and Daily Wages

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -1,19 +1,18 @@
 #!/bin/bash/
 
-# master branch
-echo "Welcome to Employee Wages Computation Program"
-# UC-1 Check Employee Attendance
-Attendance=$((1+RANDOM%2))
+echo "Welcome to Employee Wages Computation Program"	# master branch
+Attendance=$((1+RANDOM%2))	#UC-1 Attendance of employee
 
-#UC-2 Calculate Daily Employee Wages
-EMP_RATE_PER_HOUR=20
+declare -A dailyWages		#UC-9 Store Day and Daily Wages
+
+EMP_RATE_PER_HOUR=20		#UC-2 Calculate Daily Employee Wages
 FULL_TIME=8
 PART_TIME=4 	#UC-3 Add Part time Employee and Wage
 MAX_HOURS_IN_MONTH=$((FULL_TIME*EMP_RATE_PER_HOUR))
 WORKING_DAY_IN_A_MONTH=20
 NUMBER_OF_HOURS_WORKED=0
 NUMBER_OF_DAYS_WORKED=0
-function getWorkingHours(){
+function getWorkingHours(){		#UC-7 Function to get work hours
         case $1 in
                         1)
                                 WORKING_HOURS=8;
@@ -25,7 +24,7 @@ function getWorkingHours(){
                 echo $WORKING_HOURS
 }
 
-function getEmpWages()
+function getEmpWages()		#UC-8 To store Daily Wages
 {
         WORKING_HOURS=$1
         echo $(($WORKING_HOURS*$EMP_RATE_PER_HOUR))
@@ -38,8 +37,9 @@ do
 	(( NUMBER_OF_DAYS_WORKED++ ))
 	WORKING_HOURS=$( getWorkingHours $RANDOM_CHECK )
 	NUMBER_OF_HOURS_WORKED=$(($NUMBER_OF_HOURS_WORKED+$WORKING_HOURS))
-	dailyWages[$NUMBER_OF_DAYS_WORKED]=$(getEmpWages $WORKING_HOURS)
+	dailyWages["Day"$NUMBER_OF_DAYS_WORKED]=$(getEmpWages $WORKING_HOURS)
 done
 MONTHLY_WAGE=$((NUMBER_OF_HOURS_WORKED*EMP_RATE_PER_HOUR)) #UC-5 Added Monthly wage logic
 echo "Monthly Wages : $MONTHLY_WAGE"
 echo "Daily Wages: ${dailyWages[@]}"
+echo "Days : ${!dailyWages[@]}"
