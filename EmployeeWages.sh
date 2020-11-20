33fdfa996094cdfebb2a88bc426d0a3190d59103
“Kajol7052” <kajol7052@gmail.com>
2020-11-20 17:23:19 +0530

    UC-5 Added logic for Calculating Monthly Wage

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -19,6 +19,8 @@ case "$RANDOM_CHECK" in
 esac
 DAILY_WAGE=$((EMP_RATE_PER_HOUR*WORKING_HOUR))
 echo "Daily Wages : $DAILY_WAGE"
-
+WORKING_DAY_IN_A_MONTH=20
+MONTHLY_WAGE=$((DAILY_WAGE*WORKING_DAY_IN_A_MONTH))
+echo "Monthly Wages ; $MONTHLY_WAGE"
 #UC-3 Add Part time Employee and Wage
 #UC-4 Add Switch case statment for daily wage
