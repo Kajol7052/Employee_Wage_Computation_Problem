1bfae00cad4adc39bda2069c7de5f1350c0108b1
“Kajol7052” <kajol7052@gmail.com>
2020-11-20 18:23:47 +0530

    Refactored : Used Function to get Working Hours

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -2,7 +2,6 @@
 
 # master branch
 echo "Welcome to Employee Wages Computation Program"
-
 # UC-1 Check Employee Attendance
 Attendance=$((1+RANDOM%2))
 
@@ -14,16 +13,23 @@ MAX_HOURS_IN_MONTH=$((FULL_TIME*EMP_RATE_PER_HOUR))
 WORKING_DAY_IN_A_MONTH=20
 NUMBER_OF_HOURS_WORKED=0
 NUMBER_OF_DAYS_WORKED=0
-while [[ $NUMBER_OF_HOURS_WORKED -le $MAX_HOURS_IN_MONTH && $NUMBERS_OF_DAYS_WORKED -le $WORKING_DAY_IN_A_MONTH ]]   #UC-6 Updated wages based on hours and days
+function getWorkingHours(){
+        case $1 in
+                        1)
+                                WORKING_HOURS=8;
+                                ;;
+                        2)
+                                WORKING_HOURS=4
+                                ;;
+                esac
+                echo $WORKING_HOURS
+}
+
+while [[ $NUMBER_OF_HOURS_WORKED -le $MAX_HOURS_IN_MONTH && $NUMBER_OF_DAYS_WORKED -le $WORKING_DAY_IN_A_MONTH ]]   #UC-6 Updated wages based on hours and days
 do
 	RANDOM_CHECK=$((1+RANDOM%2))
-	case "$RANDOM_CHECK" in  #UC-4 Add Switch case statment for daily wages
-	1) WORKING_HOUR=$FULL_TIME
-	;;
-	2) WORKING_HOUR=$PART_TIME
-	;;
-	esac
-	((NUMBER_OF_DAYS_WORKED++))
+	(( NUMBER_OF_DAYS_WORKED++ ))
+	WORKING_HOUR=$( getWorkingHours $RANDOM_CHECK )
 	NUMBER_OF_HOURS_WORKED=$(($NUMBER_OF_HOURS_WORKED+$WORKING_HOUR))
 done
 MONTHLY_WAGE=$((NUMBER_OF_HOURS_WORKED*EMP_RATE_PER_HOUR)) #UC-5 Added Monthly wage logic
