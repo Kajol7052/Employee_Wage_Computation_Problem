d444c9fe80f198369ead4507a76dbe4cbfa6e2dd
“Kajol7052” <kajol7052@gmail.com>
2020-11-20 17:07:43 +0530

    UC-3 Added Part time Employee Wages Logic

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -9,6 +9,16 @@ Attendance=$((1+RANDOM%2))
 #UC-2 Calculate Daily Employee Wages
 EMP_RATE_PER_HOUR=20
 FULL_TIME=8
-WORKING_HOUR=$FULL_TIME
+PART_TIME=4
+RANDOM_CHECK=$((1+RANDOM%2))
+if [[ $RANDOM_CHECK -eq  1 ]]
+then
+	WORKING_HOUR=$FULL_TIME
+else
+	WORKING_HOUR=$PART_TIME
+fi
 DAILY_WAGE=$((EMP_RATE_PER_HOUR*WORKING_HOUR))
 echo "Daily Wages : $DAILY_WAGE"
+
+#UC-3 Add Part time Employee and Wage
+
